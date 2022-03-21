a29cc77e6dba1dccce59c9c2a1b5490df1c79d5a
Mingi Kim <migni4575@gmail.com>
2022-03-21 21:26:49 +0900

    feat: 로그아웃 기능 구현

--- src/views/loggedin/LoggedInView.java
+++ src/views/loggedin/LoggedInView.java
@@ -15,7 +15,80 @@ public class LoggedInView extends View {
     }
 
     @Override
-    protected void handleRequest(Integer command) {
+    protected void handleRequest(final Integer command) {
+        try {
+            switch (command) {
+                case 1: {
+                    lookupProduct();
+                    break;
+                }
+                case 2: {
+                    registerProduct();
+                    break;
+                }
+                case 3: {
+                    manageRegisterProduct();
+                    break;
+                }
+                case 4: {
+                    managePurchasedProduct();
+                    break;
+                }
+                case 5: {
+                    manageCart();
+                    break;
+                }
+                case 6: {
+                    signOut();
+                    break;
+                }
+                case 7:
+                    exit();
+                    break;
+                default:
+                    throw new IllegalArgumentException("잘못된 요청입니다.");
+            }
+        } catch (RuntimeException e) {
+            System.err.println(e.getMessage());
+            System.out.println();
+        }
+    }
+
+    private void lookupProduct() {}
+
+    private void registerProduct() {}
+
+    private void manageRegisterProduct() {}
+
+    private void managePurchasedProduct() {}
+
+    private void manageCart() {}
+
+    private void signOut() {
+        System.out.println();
+        System.out.println("-------------------------------------");
+        System.out.println("로그아웃을 하시겠습니까?");
+        System.out.println("1. 예\t 2. 아니오");
+        System.out.print(">> ");
+        handleSignOut(Integer.valueOf(scanner.nextLine()));
 
     }
+
+    private void handleSignOut(final Integer signOutCommand) {
+        switch (signOutCommand) {
+            case 1:
+                setNonLoggedInState();
+                break;
+            case 2:
+                break;
+            default:
+                throw new IllegalArgumentException("잘못된 요청입니다.");
+        }
+    }
+
+    private void setNonLoggedInState() {
+        View.user = null;
+        View.state = ViewState.Non_Logged_In;
+    }
+
 }
